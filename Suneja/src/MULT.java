62d160a734cfcdeb93537b703d7863218c158d68
Sai Thilak <Sai Thilak@SAI-THILAK>
2020-02-11 14:36:23 +0530

    first commit

---
+++ Suneja/src/MULT.java
@@ -0,0 +1,15 @@
+public class MULT implements Math
+
+{
+
+	public int calculate(int p,int q)
+
+	{
+
+		return p*q;
+
+	}
+
+
+
+}
---
+++ Suneja/src/Math.java
@@ -0,0 +1,7 @@
+public interface Math 
+
+{
+
+	public int calculate(int x,int y);
+
+}
